3ad83ca0b9c5ca961794d1f4fd17624ab351dacc
Thomas Kleiven <tkleiven@localhost.localdomain>
2017-10-25 12:34:08 +0200

    fixed splitting of training set

--- Assignment_2/assignment2.r
+++ Assignment_2/assignment2.r
@@ -20,18 +20,13 @@ g2 <- function(data){
   return(y)
 }
 
-o <- function(data){
-  y <- lm(y ~ 0 + x, data=data)
-  return(y)
-}
-
 task1i <- function(size){
   tf <- targetFunction(30)
 
   plot(tf$x, tf$y)
   abline(0.5, coef(g1(tf)), col="red")
   abline(-0.5, coef(g2(tf)), col="green")
-  abline(o(tf), col="black")
+  abline(0, 0.8)
 }
 
 
@@ -47,18 +42,18 @@ task2i <- function(size){
   e_out <- rep(0,21)
   e_val <- rep(0,21)
 
-  for( index in 1:100 ){
+  for( index in 1:10000 ){
     target_function <- targetFunction(size)
 
     for(i in 5:25){
+
       # Split to train and validation set
-      train <- subset(target_function, target_function$x %in% target_function$x[c((nrow(target_function) - i):5)])
+      train <- subset(target_function, target_function$x %in% target_function$x[sample(seq_len(nrow(target_function)),  30 - i)])
       val  <- subset(target_function, !(target_function$x %in% train$x))
 
       # Train a linear model
-      g1_train <- g1(train)
-      g2_train <- g2(train)
-      underlying_function <- lm(y ~ 0 + x, data=target_function)
+      g1_train = g1(train)
+      g2_train = g2(train)
 
       # Predict
       newdata <- data.frame(x = c(val$x))
@@ -70,17 +65,16 @@ task2i <- function(size){
 
       # Best model
       best_model <- if(mse_1 < mse_2) g1_train else g2_train
-      offset <- if(mse_1 < mse_2) 0.5 else -0.5
 
       eOut <- function(x){
-        fdiff(x, coef(underlying_function), coef(best_model), offset=offset)
+        fdiff(x, 0.8, coef(best_model), offset=0.5)
       }
 
       # Append e_val
-      if(mse_1 < mse_2) e_val[i-4] = e_val[i-4] + mse_1 else e_val[i-4] = e_val[i-4] + mse_2
+      if(mse_1 < mse_2) e_val[i-4] <- e_val[i-4] + mse_1 else e_val[i-4] <- e_val[i-4] + mse_2
 
       # Append e_out
-      e_out[i-4] <- e_val[i-4] + integrate(eOut, -1, 1)$value + 1
+      e_out[i-4] <- e_out[i-4] + integrate(eOut, -1, 1)$value + 1
 
     }
 
@@ -88,11 +82,10 @@ task2i <- function(size){
   e_val <- e_val / index
   e_out <- e_out / index
 
-  return(list(e_val, e_out))
+  #write.table(e_val, sprintf("eval.csv"), col.names=FALSE,row.names=FALSE, sep=",")
+  #write.table(e_out, sprintf("eout.csv"), col.names=FALSE,row.names=FALSE, sep=",")
 }
 
-res <- task2i(30)
-plot(res[[1]], type="l", ylim=c(0,2))
-lines(res[[2]], type="l", col="red")
+task2i(30)
 
 #warnings()
