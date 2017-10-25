85a9628f150f62f9426be6f9058c6f48e045327a
Thomas Kleiven <tkleiven@localhost.localdomain>
2017-10-25 22:09:02 +0200

    implemented cv error

--- Assignment_2/task2.r
+++ Assignment_2/task2.r
@@ -1,13 +1,19 @@
 
-
-generateDataset <- function(n){
-  x <- runif(n, min=-1, max=1)
-  x <- x[order(x)]
-  y <- sapply(sin(pi*x), function(t) t + rnorm(1, mean = 0, sd = 1))
-
-  return(data.frame(x=x, y=y))
+set.seed(420)
+func2 <- function(x, eps){
+  return(sin(pi * x) + eps)
 }
 
+generateDataset <- function(n){
+  x <- runif(n, min = -1, max = 1)
+  x <- x[order(x)]
+  e <- rnorm(n, mean = 0, sd = 1)
+  y <- func2(x, e)
+  return (data.frame(x = x, y = y))
+}
+
+
+data <- generateDataset(50)
 
 # Legendre polynomials
 legendre <- function(x,n){
@@ -34,7 +40,6 @@ legendres <- function(x,q){
     }
 
 generateLegendre <- function(x, Qf, betas){
-      #betas <- generateUniformValues(Q_f+1)
       func <- rep(0,length(x))
       for(q in 0:Qf){
           func <- func + betas[q+1] * legendres(x, q)
@@ -60,29 +65,77 @@ reguralizedEstimate <- function(data, order, lambda){
   return(w_hat)
 }
 
-betas <- runif(11, min=-1, max=1)
-reguralizedModel <- function(data, lambda, order){
+reguralizedModel <- function(data, lambda, order, est=data){
   w_hat <- reguralizedEstimate(data=data, order=order, lambda=lambda)
   j <- w_hat[1]
 
   for(i in 2:11){
-    j <- j + (w_hat[i]*(generateLegendre(data$x, 10, betas=betas)))
+    j <- j + (w_hat[i]*(legendre(est$x, i-1)))
   }
   return(j)
 }
 
 buildModels <- function(){
-  data <- generateDataset(50)
-  m1 <- reguralizedModel(data, 0, 10)
-  m2 <- reguralizedModel(data, 10, 10)
-  plot(data$x, data$y)
-  lines(data$x, sin(data$x*pi), col="red")
-  lines(data$x, m1, type="l", col="blue")
-  lines(data$x, m2, type="l")
+
+  write.table(sin(data$x*pi), "data/sin.csv", col.names=FALSE,row.names=FALSE, sep=",", append=F)
+  write.table(data$y, "data/y.csv", col.names=FALSE,row.names=FALSE, sep=",", append=F)
+  write.table(data$x, "data/x.csv", col.names=FALSE,row.names=FALSE, sep=",", append=F)
+
+  m <- reguralizedModel(data, 5, 10)
+  write.table(m, sprintf("data/model_reg_%s.csv", 5), col.names=FALSE,row.names=FALSE, sep=",", append=F)
+  m <- reguralizedModel(data, 0, 10)
+  write.table(m, sprintf("data/model_reg_%s.csv", 0), col.names=FALSE,row.names=FALSE, sep=",", append=F)
+
+
+  #m <- reguralizedModel(data, 0.1, 10)
+  #plot(data$x, data$y)
+  #lines(data$x, sin(data$x*pi), type="l")
+  #lines(data$x, m, col="red", type="l")
 }
 
 buildModels()
 
+task2ii <- function(){
+
+  #Folds
+  k <- 10
+
+  #Sample the data
+  data$id <- sample(1:k, nrow(data), replace=TRUE)
+  list <- 1:k
+  cv <- rep(NA, length(seq(0.1,10,by=0.1)))
+  s <- 1
+  for(lambda in seq(0.1,10,by=0.1)){
+    prediction <- data.frame()
+    testsetCopy <- data.frame()
+
+
+    for(i in 1:k){
+      #remove rows with id i from dataframe to create training set
+      training_set <- subset(data, id %in% list[-i])
+      val_set <- subset(data, id %in% c(i))
+
+      temp <- as.data.frame(reguralizedModel(training_set, lambda, 10, val_set))
+      prediction <- rbind(prediction, temp)
+
+      testsetCopy <- rbind(testsetCopy, as.data.frame(val_set$y))
+    }
+
+    result <- cbind(prediction, testsetCopy[, 1])
+
+    colnames(result) <- c("Predicted", "Actual")
+    result$Difference <- (abs(result$Actual - result$Predicted))^2
+
+    cv_error <- sum((result$Difference))/(nrow(result))
+    cv[s] <- cv_error
+    s <- s + 1
+  }
+  return(cv)
+}
+
+#res <- task2ii()
+#write.table(res, sprintf("data/cv_error.csv"), col.names=FALSE,row.names=FALSE, sep=",")
+#plot(seq(0.1,10,by=0.1), res, xlab="lambda", ylab="error", type="l")
 
 
 #plot(legendre(data$x, 3), type="l")
